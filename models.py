867bd3b9de78653e9dda07b37a494ac39f2f6932
Andrew Plummer <plummer574@gmail.com>
2014-08-07 23:13:17 +0100

    Allow links between pages

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
-from django.db import models
 from datetime import datetime
+import re
+
+from django.db import models
+from django.utils.html import escape
+from django.core.urlresolvers import reverse
 
 
 class Page(models.Model):
@@ -13,3 +17,10 @@ class Page(models.Model):
             self.created_at = datetime.today()
         self.updated_at = datetime.today()
         return super(Page, self).save(*args, **kwargs)
+
+    def as_html(self):
+        def match_replace(match):
+            val = match.group(1)
+            return '<a href="{0}">{1}</a>'.format(
+                reverse('mainstay_wiki:page', args=(val,)), escape(val))
+        return re.sub(r'\[\[(.*)\]\]', match_replace, escape(self.content))
